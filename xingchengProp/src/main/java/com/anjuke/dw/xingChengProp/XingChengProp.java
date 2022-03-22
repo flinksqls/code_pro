eb7bfa8551b5c3ecb9cbac86debdd915b6b37774
jiangyi02 <jiangyi02@58.com>
2022-03-22 23:41:19 +0800

    上传模块

--- xingchengProp/src/main/java/com/anjuke/dw/xingChengProp/XingChengProp.java
+++ xingchengProp/src/main/java/com/anjuke/dw/xingChengProp/XingChengProp.java
@@ -3,5 +3,6 @@ package com.anjuke.dw.xingChengProp;
 public class XingChengProp {
     public static void main(String[] args) {
         System.out.println("hello git !");
+        System.out.println("hello flink !");
     }
 }
