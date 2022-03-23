cdf8d9cf9d2fda2faeaf20a6edac6f6e7f4f40ab
jiangyi02 <jiangyi02@58.com>
2022-03-23 17:54:01 +0800

    master rebase

--- xingchengProp/src/main/java/com/anjuke/dw/xingChengProp/Demo.java
+++ xingchengProp/src/main/java/com/anjuke/dw/xingChengProp/Demo.java
@@ -3,5 +3,6 @@ package com.anjuke.dw.xingChengProp;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("hi git rebase !");
+        System.out.println("haha rebase ！");
     }
 }
